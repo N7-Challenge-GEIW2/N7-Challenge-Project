823a8e721b1059c7758ea9c57e4f56ea60b579bc
younes makhchan <makhchaney@cyberdefenders.org>
2024-05-11 11:53:50 +0100

    fixing the folder  naming

--- system-student/frontend/src/app/app.module.ts
+++ system-student/frontend/src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppErrorsComponent } from './app-errors/app-errors.component';
+import { NotAuthorizedComponent } from './not-authorized/not-authorized.component';
+import { StudentTemplateComponent } from './student-template/student-template.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {AppHttpInterceptor} from "./services/app-http.interceptor";
+import { TeacherTemplateComponent } from './teacher-template/teacher-template.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    AppErrorsComponent,
+    NotAuthorizedComponent,
+    StudentTemplateComponent,
+    TeacherTemplateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide : HTTP_INTERCEPTORS, useClass : AppHttpInterceptor, multi : true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
